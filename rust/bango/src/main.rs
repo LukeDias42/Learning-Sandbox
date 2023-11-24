93d82e19347d1ba249ad8c9e62972fab23c1ed49
Lucas Dias <lhollowwizard@gmail.com>
2023-11-24 16:40:37 -0300

    feat: made a simple bingo application

---
+++ rust/bango/src/main.rs
@@ -0,0 +1,53 @@
+use shuffle::shuffler::Shuffler;
+use shuffle::irs::Irs;
+use std::io;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut irs = Irs::<u8>::default();
+
+    let mut input = Vec::new();
+    for i in 1..=75 {
+        input.push(i);
+    }
+
+    _ = irs.shuffle(&mut input, &mut rng);
+
+    let mut enter = String::new();
+    let reader = io::stdin();
+    let mut drawn_numbers = Vec::new();
+    for i in 0..75 {
+        drawn_numbers.push(input[i]);
+        print_numbers(&drawn_numbers, input[i]);
+        reader.read_line(&mut enter)
+            .expect("Error");
+    }
+}
+
+fn print_numbers(drawn_numbers: &Vec<u8>, last_drawn: u8) {
+    print!("\x1B[2J\x1B[1;1H");
+    for i in 1..=75 {
+        if drawn_numbers.contains(&(i as u8)) {
+            print!("\x1b[1;32m");
+            if i < 10 {
+                print!("0{i} ");
+            }
+            else {
+                print!("{i} ");
+            }
+            print!("\x1b[1;39m");
+        }
+        else {
+            print!("\x1b[1;31m");
+            print!("__ ");
+            print!("\x1b[1;39m");
+        }
+        if i % 5 == 0 {
+            print!("\n");
+        }
+        if i % 15 == 0 {
+            print!("\n");
+        }
+    }
+    println!("Número Sorteado: {last_drawn}")
+}
