44042b30d4a9a550795bcab66fc4f3f919aef51c
Luke Dias <lhollowwizard@gmail.com>
2024-08-16 10:12:19 -0300

    feat: clippy suggestions

--- Rust/bango/src/main.rs
+++ Rust/bango/src/main.rs
@@ -1,5 +1,5 @@
-use shuffle::shuffler::Shuffler;
 use shuffle::irs::Irs;
+use shuffle::shuffler::Shuffler;
 use std::io;
 
 fn main() {
@@ -16,37 +16,35 @@ fn main() {
     let mut enter = String::new();
     let reader = io::stdin();
     let mut drawn_numbers = Vec::new();
-    for i in 0..75 {
-        drawn_numbers.push(input[i]);
-        print_numbers(&drawn_numbers, input[i]);
-        reader.read_line(&mut enter)
-            .expect("Error");
+    for value in input {
+        drawn_numbers.push(value);
+        print_numbers(&drawn_numbers, value);
+        reader.read_line(&mut enter).expect("Error");
     }
 }
 
-fn print_numbers(drawn_numbers: &Vec<u8>, last_drawn: u8) {
+fn print_numbers(drawn_numbers: &[u8], last_drawn: u8) {
     print!("\x1B[2J\x1B[1;1H");
     for i in 1..=75 {
         if drawn_numbers.contains(&(i as u8)) {
             print!("\x1b[1;32m");
             if i < 10 {
-                print!("0{i} ");
-            }
-            else {
-                print!("{i} ");
+                print!("0{} ", i);
+            } else {
+                print!("{} ", i);
             }
             print!("\x1b[1;39m");
-        }
-        else {
+        } else {
             print!("\x1b[1;31m");
             print!("__ ");
             print!("\x1b[1;39m");
         }
+
         if i % 5 == 0 {
-            print!("\n");
+            println!();
         }
         if i % 15 == 0 {
-            print!("\n");
+            println!();
         }
     }
     println!("Número Sorteado: {last_drawn}")
