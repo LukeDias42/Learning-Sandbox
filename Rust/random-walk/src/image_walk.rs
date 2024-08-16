cb124f70e6ef3592a958360835d4608979fb3d76
Luke Dias <lhollowwizard@gmail.com>
2024-08-16 09:43:31 -0300

    feat: clean random walk
    Simplified logic to get color on image walk and ran a fmt.

--- Rust/random-walk/src/image_walk.rs
+++ Rust/random-walk/src/image_walk.rs
@@ -1,35 +1,36 @@
-use super::splitmix::SplitMix;
 use super::move_square::update_position;
-use image::{ RgbImage, Rgb };
-use stopwatch::{ Stopwatch };
+use super::splitmix::SplitMix;
+use image::{Rgb, RgbImage};
+use stopwatch::Stopwatch;
 
 pub fn random_image(
-    file_name: &str, 
-    width: u32, 
-    height: u32, 
-    repetitions: u64, 
+    file_name: &str,
+    width: u32,
+    height: u32,
+    repetitions: u64,
     square_size: isize,
-    mut color_change_speed: i64) {
-    let mut img = RgbImage::from_fn(
-        width, 
-        height, 
-        |_,_| Rgb([0x0, 0x0, 0x0])
-    );
+    mut color_change_speed: i64,
+) {
+    let mut img = RgbImage::from_fn(width, height, |_, _| Rgb([0x0, 0x0, 0x0]));
 
     let mut hasher = SplitMix::new();
-    let mut pos = (hasher.rand(width as u64) as isize, 
-                   hasher.rand(height as u64) as isize);
-    let mut color_value = 0xff_ff_ff;
+    let mut pos = (
+        hasher.rand(width as u64) as isize,
+        hasher.rand(height as u64) as isize,
+    );
+    let mut color_value: i64 = 0xff_ff_ff;
     println!("{repetitions} movements started.");
     let sw = Stopwatch::start_new();
 
     for _ in 0..repetitions {
         let c = Rgb([
-                ((color_value >> 0x10 & 0xff) >> 0) as u8, 
-                ((color_value >> 0x08 & 0xff) >> 0) as u8, 
-                ((color_value >> 0x00 & 0xff) >> 0) as u8]
-        );
-        if color_value <= 0 || color_value >= 0xff_ff_ff { color_change_speed *= -1i64; }
+            (color_value >> 0x08 & 0xff) as u8,
+            (color_value >> 0x04 & 0xff) as u8,
+            (color_value & 0xff) as u8,
+        ]);
+        if color_value <= 0 || color_value >= 0xff_ff_ff {
+            color_change_speed *= -1i64;
+        }
         color_value += color_change_speed;
         for i in 0..square_size as u32 {
             for j in 0..square_size as u32 {
@@ -40,14 +41,7 @@ pub fn random_image(
     }
 
     println!("{} ms to complete.", sw.elapsed_ms());
-    img.save(format!("{file_name}.png")).unwrap_or_else(|err| {
-        println!("An error occured while trying to save the image: {err}");
-        return;
-    });
-    println!("Saved image: {file_name}.png.");
+    img.save(format!("{file_name}.png"))
+        .unwrap_or_else(|err| println!("An error occured while trying to save the image: {err}"));
+    println!("Saved image: {file_name}.png.")
 }
-
-fn draw_square(size: u32, pos: (u32, u32), img: &mut RgbImage, color: &Rgb<u8>) {
-
-}
-
