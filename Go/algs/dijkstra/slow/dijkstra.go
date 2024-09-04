826019456cbea9438d5170844a7966a080d56519
Luke Dias <lhollowwizard@gmail.com>
2024-09-04 20:51:01 -0300

    feat: implemented dijkstra without priority queue
    Also fixed get path that was returning the reversed correct path

--- Go/algs/dijkstra/slow/dijkstra.go
+++ Go/algs/dijkstra/slow/dijkstra.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
-
-type vertex struct {
-	node
-	distance float64
-}
 
 type node struct {
 	city string
 }
 
-func dijkstra(graph map[node][]vertex, src node, dest node) []node {
+func dijkstra(graph map[node]map[node]float64, src node, dest node) []node {
 	unvisited := make(map[node]bool)
 	distances := make(map[node]float64)
 	predecessors := make(map[node]node)
-	fmt.Println(predecessors)
 
 	for k := range graph {
 		distances[k] = math.MaxFloat64
@@ -27,6 +20,19 @@ func dijkstra(graph map[node][]vertex, src node, dest node) []node {
 	distances[src] = 0
 
 	for len(unvisited) > 0 {
+		closestNode := nextClosestNode(distances, unvisited)
+		unvisited[closestNode] = false
+		if closestNode == dest {
+			return getPath(dest, predecessors)
+		}
+		for neighbor := range graph[closestNode] {
+			totalDist := distances[closestNode] + graph[closestNode][neighbor]
+			if distances[neighbor] > totalDist {
+				distances[neighbor] = totalDist
+				predecessors[neighbor] = closestNode
+
+			}
+		}
 	}
 
 	return []node{}
@@ -47,9 +53,9 @@ func nextClosestNode(distances map[node]float64, unvisited map[node]bool) node {
 	return closestNode
 }
 
-func getPath(source node, predecessors map[node]node) []node {
+func getPath(dest node, predecessors map[node]node) []node {
 	path := []node{}
-	current := source
+	current := dest
 	emptyNode := node{}
 	for current != emptyNode {
 		path = append(path, current)
@@ -58,6 +64,10 @@ func getPath(source node, predecessors map[node]node) []node {
 		if !ok {
 			current = emptyNode
 		}
+	}
+	for i := 0; i < len(path)/2; i++ {
+		j := len(path) - i - 1
+		path[i], path[j] = path[j], path[i]
 	}
 	return path
 }
