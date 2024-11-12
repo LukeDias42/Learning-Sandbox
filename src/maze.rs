d755d2793ecd7a0fb28659248905e36271a5b364
Luke Dias <lhollowwizard@gmail.com>
2024-11-12 11:36:55 -0300

    feat(maze): implement create cells
    Create cells generate an amount of cells based on the maze rows and
    columns and sizes.

--- src/maze.rs
+++ src/maze.rs
@@ -28,4 +28,27 @@ impl Maze {
         maze.break_entrance_and_exit();
         maze
     }
+    fn create_cells(
+        start: Point,
+        rows: usize,
+        columns: usize,
+        cell_width: usize,
+        cell_height: usize,
+    ) -> Vec<Vec<Cell>> {
+        let mut cells: Vec<Vec<Cell>> = Vec::with_capacity(rows);
+        for y in 0..rows {
+            let mut row: Vec<Cell> = Vec::with_capacity(columns);
+            for x in 0..columns {
+                let diagonal =
+                    Point::new(x * cell_width + start.x, y * cell_height + start.y).to(Point::new(
+                        start.x + (x + 1) * cell_width,
+                        start.y + (y + 1) * cell_height,
+                    ));
+                let cell = Cell::new(true, true, true, true, diagonal);
+                row.push(cell);
+            }
+            cells.push(row);
+        }
+        cells
+    }
 }
