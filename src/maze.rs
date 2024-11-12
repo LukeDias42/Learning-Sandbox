91b5ba01482de381d6e956ffd7ec90f976ce2196
Luke Dias <lhollowwizard@gmail.com>
2024-11-12 11:43:55 -0300

    feat(maze): implement break walls recursive
    Each cell chooses a random side to go to in order to generate the maze,
    when there is nowhere else for the cell to go, it goes back to the next
    random direction.

--- src/maze.rs
+++ src/maze.rs
@@ -74,4 +74,68 @@ impl Maze {
         self.cells[self.rows - 1][self.columns - 1].sides = [true, true, true, false];
     }
 
+    pub fn break_walls_r(&mut self, x: usize, y: usize, window_width: usize, window_height: usize) {
+        let mut rng = rand::thread_rng();
+
+        self.cells[y][x].visited = true;
+        let mut to_visit: Vec<[isize; 2]> = Vec::with_capacity(4);
+
+        if y != 0 && !self.cells[y - 1][x].visited {
+            to_visit.push([0, -1]); // Up
+        }
+        if y < self.rows - 1 && !self.cells[y + 1][x].visited {
+            to_visit.push([0, 1]); // Down
+        }
+        if x != 0 && !self.cells[y][x - 1].visited {
+            to_visit.push([-1, 0]); // Left
+        }
+        if x < self.columns - 1 && !self.cells[y][x + 1].visited {
+            to_visit.push([1, 0]); // Right
+        }
+
+        if to_visit.is_empty() {
+            return;
+        }
+
+        while !to_visit.is_empty() {
+            let random_direction = rng.gen_range(0..to_visit.len());
+            let direction = to_visit[random_direction];
+
+            let new_x = ((x as isize) + direction[0]) as usize;
+            let new_y = ((y as isize) + direction[1]) as usize;
+
+            if self.cells[new_y][new_x].visited {
+                to_visit.remove(random_direction);
+                continue;
+            }
+            match direction {
+                [1, 0] => {
+                    self.cells[y][x].sides[2] = false; // Remove right wall of current cell
+                    self.cells[new_y][new_x].sides[0] = false; // Remove left wall of new cell
+                }
+                [-1, 0] => {
+                    // Moving left
+                    self.cells[y][x].sides[0] = false; // Remove left wall of current cell
+                    self.cells[new_y][new_x].sides[2] = false; // Remove right wall of new cell
+                }
+                [0, 1] => {
+                    // Moving down
+                    self.cells[y][x].sides[3] = false; // Remove bottom wall of current cell
+                    self.cells[new_y][new_x].sides[1] = false; // Remove top wall of new cell
+                }
+                [0, -1] => {
+                    // Moving up
+                    self.cells[y][x].sides[1] = false; // Remove top wall of current cell
+                    self.cells[new_y][new_x].sides[3] = false; // Remove bottom wall of new cell
+                }
+                _ => {}
+            }
+
+            self.break_walls_r(new_x, new_y, window_width, window_height);
+
+            // Remove the visited direction from the list
+            to_visit.remove(random_direction);
+        }
+    }
+
 }
