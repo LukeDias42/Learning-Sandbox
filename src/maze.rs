8eb4b61b2c9003c360b8370bdc73ec37322243f1
Luke Dias <lhollowwizard@gmail.com>
2024-11-12 11:35:59 -0300

    feat(maze): implement maze new

--- src/maze.rs
+++ src/maze.rs
@@ -8,3 +8,24 @@ pub struct Maze {
     pub cells: Vec<Vec<Cell>>,
 }
 
+impl Maze {
+    pub fn new(
+        start: Point,
+        rows: usize,
+        columns: usize,
+        cell_width: usize,
+        cell_height: usize,
+    ) -> Maze {
+        let cells = Maze::create_cells(start, rows, columns, cell_width, cell_height);
+        let mut maze = Maze {
+            start,
+            rows,
+            columns,
+            cell_width,
+            cell_height,
+            cells,
+        };
+        maze.break_entrance_and_exit();
+        maze
+    }
+}
