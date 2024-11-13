f4617fe35e754dde002549c820ca895e13443382
Luke Dias <lhollowwizard@gmail.com>
2024-11-13 01:40:06 -0300

    feat(maze): allow any starting and end points

--- Rust/maze-solver/src/maze.rs
+++ Rust/maze-solver/src/maze.rs
@@ -12,6 +12,8 @@ pub struct Maze {
     pub cell_width: usize,
     pub cell_height: usize,
     pub cells: Vec<Vec<Cell>>,
+    start_cell: Point,
+    end_cell: Point,
 }
 
 impl Maze {
@@ -23,6 +25,7 @@ impl Maze {
         cell_height: usize,
     ) -> Maze {
         let cells = Maze::create_cells(start, rows, columns, cell_width, cell_height);
+        let [start_cell, end_cell] = Maze::choose_start_and_end_cell(columns, rows);
         let mut maze = Maze {
             start,
             rows,
@@ -30,10 +33,35 @@ impl Maze {
             cell_width,
             cell_height,
             cells,
+            start_cell,
+            end_cell,
         };
         maze.break_entrance_and_exit();
         maze
     }
+    fn choose_start_and_end_cell(width: usize, height: usize) -> [Point; 2] {
+        let start_cell = Maze::pick_random_outer_cell(width, height);
+        let mut end_cell = Maze::pick_random_outer_cell(width, height);
+        while start_cell == end_cell {
+            end_cell = Maze::pick_random_outer_cell(width, height);
+        }
+        return [start_cell, end_cell];
+    }
+    fn pick_random_outer_cell(width: usize, height: usize) -> Point {
+        let mut rng = rand::thread_rng();
+        let random_direction = rng.gen_range(0..=3);
+        if random_direction == 0 {
+            return Point::new(0, rng.gen_range(0..height));
+        }
+        if random_direction == 1 {
+            return Point::new(rng.gen_range(0..width), 0);
+        }
+        if random_direction == 2 {
+            return Point::new(width - 1, rng.gen_range(0..height));
+        }
+        Point::new(rng.gen_range(0..width), height - 1)
+    }
+
     fn create_cells(
         start: Point,
         rows: usize,
@@ -69,12 +97,36 @@ impl Maze {
         let opening_color = Color::new(0x33, 0x33, 0x33);
         self.cells[y][x].plot_cell_lines(color, opening_color, buffer, window_width, window_height);
     }
-    pub fn break_entrance_and_exit(&mut self) {
-        self.cells[0][0].sides = [true, false, true, true];
-        self.cells[self.rows - 1][self.columns - 1].sides = [true, true, true, false];
+    fn break_entrance_and_exit(&mut self) {
+        self.cells[self.start_cell.y][self.start_cell.x].sides =
+            self.walls_based_on_outer_direction(self.start_cell.x, self.start_cell.y);
+        self.cells[self.end_cell.y][self.end_cell.x].sides =
+            self.walls_based_on_outer_direction(self.end_cell.x, self.end_cell.y);
     }
 
-    pub fn break_walls_r(&mut self, x: usize, y: usize, window_width: usize, window_height: usize) {
+    fn walls_based_on_outer_direction(&self, x: usize, y: usize) -> [bool; 4] {
+        if x == 0 {
+            return [false, true, true, true];
+        }
+        if y == 0 {
+            return [true, false, true, true];
+        }
+        if x == self.columns - 1 {
+            return [true, true, false, true];
+        }
+        return [true, true, true, false];
+    }
+
+    pub fn break_walls(&mut self, window_width: usize, window_height: usize) {
+        self.break_walls_r(
+            self.start_cell.x,
+            self.start_cell.y,
+            window_width,
+            window_height,
+        )
+    }
+
+    fn break_walls_r(&mut self, x: usize, y: usize, window_width: usize, window_height: usize) {
         let mut rng = rand::thread_rng();
 
         self.cells[y][x].visited = true;
@@ -153,9 +205,17 @@ impl Maze {
         window_height: usize,
         window: &mut Window,
     ) -> bool {
-        self.solve_r(0, 0, buffer, window_width, window_height, window)
+        self.solve_r(
+            self.start_cell.x,
+            self.start_cell.y,
+            buffer,
+            window_width,
+            window_height,
+            window,
+        )
     }
-    pub fn solve_r(
+
+    fn solve_r(
         &mut self,
         x: usize,
         y: usize,
@@ -165,27 +225,31 @@ impl Maze {
         window: &mut Window,
     ) -> bool {
         self.cells[y][x].visited = true;
-        if x == self.columns - 1 && y == self.rows - 1 {
+        if x == self.end_cell.x && y == self.end_cell.y {
             return true;
         }
 
         let mut not_visited: Vec<[isize; 2]> = Vec::with_capacity(4);
         if x != 0 && !self.cells[y][x].sides[0] {
+            // left
             if !self.cells[y][x - 1].visited {
                 not_visited.push([-1, 0]);
             }
         }
         if y != 0 && !self.cells[y][x].sides[1] {
+            // up
             if !self.cells[y - 1][x].visited {
                 not_visited.push([0, -1]);
             }
         }
         if x < self.columns - 1 && !self.cells[y][x].sides[2] {
+            // right
             if !self.cells[y][x + 1].visited {
                 not_visited.push([1, 0]);
             }
         }
         if y < self.rows - 1 && !self.cells[y][x].sides[3] {
+            // down
             if !self.cells[y + 1][x].visited {
                 not_visited.push([0, 1]);
             }
