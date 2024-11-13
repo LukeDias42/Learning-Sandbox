834b32cb8a0d0c1d856e8c0cb9599ea3a8e6fcba
Luke Dias <lhollowwizard@gmail.com>
2024-11-13 01:46:02 -0300

    feat(maze_hex): implement choose start and end cell based on maze implementation

--- Rust/maze-solver/src/maze_hex.rs
+++ Rust/maze-solver/src/maze_hex.rs
@@ -16,3 +16,28 @@ pub struct MazeHex {
 }
 
 const SQRT_3: f64 = 1.732050807568877293527446341505872367_f64;
+impl MazeHex {
+    fn choose_start_and_end_cell(width: usize, height: usize) -> [Point; 2] {
+        let start_cell = MazeHex::pick_random_outer_cell(width, height);
+        let mut end_cell = MazeHex::pick_random_outer_cell(width, height);
+        while start_cell == end_cell {
+            end_cell = MazeHex::pick_random_outer_cell(width, height);
+        }
+        return [start_cell, end_cell];
+    }
+    fn pick_random_outer_cell(width: usize, height: usize) -> Point {
+        let mut rng = rand::thread_rng();
+        let random_direction = rng.gen_range(0..=3);
+        if random_direction == 0 {
+            return Point::new(0, rng.gen_range(0..height));
+        }
+        if random_direction == 1 {
+            return Point::new(rng.gen_range(0..width), 0);
+        }
+        if random_direction == 2 {
+            return Point::new(width - 1, rng.gen_range(0..height));
+        }
+        Point::new(rng.gen_range(0..width), height - 1)
+    }
+
+}
