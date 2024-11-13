512599438113c2144a49e65709d8d619637b96ce
Luke Dias <lhollowwizard@gmail.com>
2024-11-13 01:42:34 -0300

    feat(cell_hex): implement plot cell
    This function uses the horizontal diagonal to draws the six lines that
    makes for a hexagon.

--- Rust/maze-solver/src/cell_hex.rs
+++ Rust/maze-solver/src/cell_hex.rs
@@ -1,3 +1,6 @@
+use crate::color::Color;
+use crate::line::Line;
+use crate::point::Point;
 
 pub struct CellHex {
     pub sides: [bool; 6],
@@ -6,6 +9,7 @@ pub struct CellHex {
     pub visited: bool,
 }
 
+const SQRT_3: f64 = 1.732050807568877293527446341505872367_f64;
 impl CellHex {
     pub fn new(
         has_down_left_wall: bool,
@@ -46,6 +50,80 @@ impl CellHex {
             side_len,
             horizontal_diagonal: normalized_diagonal,
             visited: false,
+        }
+    }
+    pub fn plot_cell_lines(
+        &self,
+        wall_color: Color,
+        opening_color: Color,
+        buffer: &mut Vec<u32>,
+        window_width: usize,
+        window_height: usize,
+    ) {
+        let mut lines = Vec::with_capacity(6);
+        let half_height = SQRT_3 * self.side_len / 2_f64;
+
+        // down left - 0
+        lines.push(Line::new(
+            Point::new(self.horizontal_diagonal.p1.x, self.horizontal_diagonal.p1.y),
+            Point::new(
+                self.horizontal_diagonal.p1.x + (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 + half_height) as usize,
+            ),
+        ));
+        // up left - 1
+        lines.push(Line::new(
+            Point::new(self.horizontal_diagonal.p1.x, self.horizontal_diagonal.p1.y),
+            Point::new(
+                self.horizontal_diagonal.p1.x + (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 - half_height) as usize,
+            ),
+        ));
+        // up - 2
+        lines.push(Line::new(
+            Point::new(
+                self.horizontal_diagonal.p1.x + (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 - half_height) as usize,
+            ),
+            Point::new(
+                self.horizontal_diagonal.p1.x + (3_f64 * self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 - half_height) as usize,
+            ),
+        ));
+        // up right - 3
+        lines.push(Line::new(
+            Point::new(self.horizontal_diagonal.p2.x, self.horizontal_diagonal.p2.y),
+            Point::new(
+                self.horizontal_diagonal.p2.x - (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p2.y as f64 - half_height) as usize,
+            ),
+        ));
+        // down right - 4
+        lines.push(Line::new(
+            Point::new(self.horizontal_diagonal.p2.x, self.horizontal_diagonal.p2.y),
+            Point::new(
+                self.horizontal_diagonal.p2.x - (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p2.y as f64 + half_height) as usize,
+            ),
+        ));
+        // down - 5
+        lines.push(Line::new(
+            Point::new(
+                self.horizontal_diagonal.p1.x + (self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 + half_height) as usize,
+            ),
+            Point::new(
+                self.horizontal_diagonal.p1.x + (3_f64 * self.side_len / 2_f64) as usize,
+                (self.horizontal_diagonal.p1.y as f64 + half_height) as usize,
+            ),
+        ));
+        for wall in 0..self.sides.len() {
+            let color = if self.sides[wall] {
+                wall_color
+            } else {
+                opening_color
+            };
+            lines[wall].plot_bresenham_line(&color, buffer, window_width, window_height);
         }
     }
 }
