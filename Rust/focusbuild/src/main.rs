a3e3f8a9723f441080535341e919b689f9f19976
Luke Dias <lhollowwizard@gmail.com>
2024-11-20 13:24:38 -0300

    feat(focusbuild): remove initialization of the terminal on the main

--- Rust/focusbuild/src/main.rs
+++ Rust/focusbuild/src/main.rs
@@ -7,20 +7,12 @@ use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{backend::CrosstermBackend, layout::Rect, Terminal, TerminalOptions, Viewport};
 use std::io::stdout;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     color_eyre::install()?;
-    let backend = CrosstermBackend::new(stdout());
-    let terminal_size = Terminal::new(backend)?.size()?;
-    let width = terminal_size.width;
-    let height = terminal_size.height;
-
-    let viewport = Viewport::Fixed(Rect::new(0, 0, width, height));
-    let terminal = ratatui::init_with_options(TerminalOptions { viewport });
     execute!(stdout(), EnterAlternateScreen).expect("failed to enter alternate screen");
-    let _ = App::new()?.run(terminal);
+    let _ = App::new()?.run();
     execute!(stdout(), LeaveAlternateScreen).expect("failed to leave alternate screen");
 
     ratatui::restore();
