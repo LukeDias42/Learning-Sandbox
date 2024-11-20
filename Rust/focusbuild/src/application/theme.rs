1b8c27de24b5a0c88aba8a9ab858ee012e4c78cb
Luke Dias <lhollowwizard@gmail.com>
2024-11-20 12:45:16 -0300

    feat(theme): changed themes

--- Rust/focusbuild/src/application/theme.rs
+++ Rust/focusbuild/src/application/theme.rs
@@ -1,17 +1,16 @@
 use ratatui::style::{Color, Style};
-
 pub struct Theme {
     pub root: Style,
-    pub borders: Style,
     pub key_binding: KeyBinding,
     pub logo: Style,
     pub timer: Timer,
-    pub statistics: Statistics,
+    pub history: History,
 }
 
 pub struct KeyBinding {
     pub key: Style,
     pub description: Style,
+    pub surrounding: Style,
 }
 
 pub struct Timer {
@@ -20,37 +19,39 @@ pub struct Timer {
     pub title: Style,
 }
 
-pub struct Statistics {
+pub struct History {
     pub style: Style,
-    pub title: Style,
+    pub border: Style,
 }
 
 pub const THEME: Theme = Theme {
     root: Style::new().bg(CHARCOAL_BROWN),
-    borders: Style::new().fg(LIGHT_GRAY),
     logo: Style::new().fg(MOSS_GREEN),
     key_binding: KeyBinding {
-        key: Style::new().fg(BLACK).bg(DARK_GRAY),
-        description: Style::new().fg(DARK_GRAY).bg(BLACK),
+        key: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_DARK),
+        description: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_LIGHT),
+        surrounding: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_LIGHT),
     },
     timer: Timer {
-        active: Style::new().bg(DARK_BLUE).fg(MOSS_GREEN),
-        inactive: Style::new().bg(LIGHT_RED).fg(MOSS_GREEN),
+        active: Style::new().bg(LIGHT_GREEN).fg(MOSS_GREEN),
+        inactive: Style::new().bg(CHARCOAL_BROWN).fg(MOSS_GREEN),
         title: Style::new().fg(MOSS_GREEN),
     },
-    statistics: Statistics {
-        style: Style::new().bg(DARK_BLUE).fg(LIGHT_RED),
-        title: Style::new().fg(MOSS_GREEN),
+    history: History {
+        style: Style::new().bg(CHARCOAL_BROWN).fg(MOSS_GREEN),
+        border: Style::new().fg(MOSS_GREEN),
     },
 };
 
 pub const CHARCOAL_BROWN: Color = Color::Rgb(60, 48, 42);
+pub const CHARCOAL_BROWN_DARK: Color = Color::Rgb(40, 28, 22);
+pub const CHARCOAL_BROWN_LIGHT: Color = Color::Rgb(80, 68, 62);
 pub const MOSS_GREEN: Color = Color::Rgb(138, 154, 91);
-pub const LIGHT_GRAY: Color = Color::Rgb(0xC2, 0xC2, 0xC2);
-pub const LIGHT_RED: Color = Color::Rgb(0xFF, 0xC2, 0xC2);
-// pub const LIGHT_GREEN: Color = Color::Rgb(0xC2, 0xFF, 0xC2);
+// pub const LIGHT_GRAY: Color = Color::Rgb(0xC2, 0xC2, 0xC2);
+// pub const LIGHT_RED: Color = Color::Rgb(0xFF, 0xC2, 0xC2);
+pub const LIGHT_GREEN: Color = Color::Rgb(0xC2, 0xFF, 0xC2);
 // pub const LIGHT_BLUE: Color = Color::Rgb(0xC2, 0xC2, 0xFF);
-pub const DARK_GRAY: Color = Color::Rgb(0x22, 0x22, 0x22);
-pub const BLACK: Color = Color::Rgb(0x0, 0x0, 0x0);
+// pub const DARK_GRAY: Color = Color::Rgb(0x22, 0x22, 0x22);
+// pub const BLACK: Color = Color::Rgb(0x0, 0x0, 0x0);
 // pub const WHITE: Color = Color::Rgb(0xFF, 0xFF, 0xFF);
-pub const DARK_BLUE: Color = Color::Rgb(0x42, 0x42, 0xFF);
+// pub const DARK_BLUE: Color = Color::Rgb(0x42, 0x42, 0xFF);
