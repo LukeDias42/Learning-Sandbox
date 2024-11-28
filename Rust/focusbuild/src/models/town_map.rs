77e73d9950a66b38c1181e26cf8796b34124dd20
Luke Dias <lhollowwizard@gmail.com>
2024-11-28 18:24:10 -0300

    feat(town_map): add object enum

--- Rust/focusbuild/src/models/town_map.rs
+++ Rust/focusbuild/src/models/town_map.rs
@@ -1,4 +1,20 @@
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum Object {
+    #[default]
+    None,
+    River,
+    House,
+    HouseWithGarden,
+    Office,
+    Tree,
+    Tree2,
+    Palm,
+    Grass,
+    Cactus,
+    Road,
+}
+
 pub enum Floor {
     #[default]
     Grass,
