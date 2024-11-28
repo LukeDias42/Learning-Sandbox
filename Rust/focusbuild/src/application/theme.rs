c212abdc15ece2077d3a60ff8c94698f0c0c1beb
Luke Dias <lhollowwizard@gmail.com>
2024-11-28 18:27:49 -0300

    feat(theme): changed themes
    Changed colors and removed the background

--- Rust/focusbuild/src/application/theme.rs
+++ Rust/focusbuild/src/application/theme.rs
@@ -1,11 +1,11 @@
-use ratatui::style::{palette::material::WHITE, Color, Style};
+use ratatui::style::{Color, Style};
 pub struct Theme {
-    pub root: Style,
     pub key_binding: KeyBinding,
     pub logo: Style,
     pub timer: Timer,
     pub history: History,
     pub data: Data,
+    pub town: Town,
 }
 
 pub struct KeyBinding {
@@ -31,40 +31,50 @@ pub struct History {
     pub border: Style,
 }
 
+pub struct Town {
+    pub river: Style,
+    pub grass: Style,
+    pub city: Style,
+    pub earth: Style,
+    pub desert: Style,
+}
+
 pub const THEME: Theme = Theme {
-    root: Style::new().bg(CHARCOAL_BROWN).fg(CHARCOAL_BROWN),
-    logo: Style::new().bg(CHARCOAL_BROWN).fg(MOSS_GREEN),
+    logo: Style::new().fg(EARTH_RED),
     key_binding: KeyBinding {
-        key: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_DARK),
-        description: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_LIGHT),
-        surrounding: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN_LIGHT),
-        block: Style::new().fg(MOSS_GREEN).bg(CHARCOAL_BROWN),
+        key: Style::new().fg(EARTH_RED).bg(CHARCOAL_BROWN_DARK),
+        description: Style::new().fg(EARTH_RED).bg(Color::Reset),
+        surrounding: Style::new().fg(EARTH_RED),
+        block: Style::new().fg(EARTH_RED),
     },
     timer: Timer {
-        active: Style::new().bg(LIGHT_GREEN).fg(MOSS_GREEN),
-        inactive: Style::new().bg(CHARCOAL_BROWN).fg(MOSS_GREEN),
-        title: Style::new().fg(MOSS_GREEN),
+        active: Style::new().bg(DARK_MOSS_GREEN).fg(EARTH_RED),
+        inactive: Style::new().fg(EARTH_RED),
+        title: Style::new().fg(EARTH_RED),
     },
     history: History {
-        style: Style::new().bg(CHARCOAL_BROWN).fg(MOSS_GREEN),
-        border: Style::new().fg(MOSS_GREEN),
+        style: Style::new().fg(EARTH_RED),
+        border: Style::new().fg(EARTH_RED),
     },
     data: Data {
-        break_bar: Style::new().bg(CHARCOAL_BROWN).fg(LIGHT_RED),
-        focus_bar: Style::new().bg(CHARCOAL_BROWN).fg(LIGHT_GREEN),
-        block: Style::new().bg(LIGHT_RED).fg(WHITE),
+        break_bar: Style::new().fg(LIGHT_RED),
+        focus_bar: Style::new().fg(FOCUS_GREEN),
+        block: Style::new().fg(Color::White),
+    },
+    town: Town {
+        river: Style::new().fg(DEEP_BLUE).bg(Color::Cyan),
+        grass: Style::new().bg(Color::LightGreen),
+        city: Style::new().bg(Color::DarkGray),
+        earth: Style::new().bg(CHARCOAL_BROWN),
+        desert: Style::new().bg(LIGHT_SAND_COLOR),
     },
 };
 
-pub const CHARCOAL_BROWN: Color = Color::Rgb(60, 48, 42);
-pub const CHARCOAL_BROWN_DARK: Color = Color::Rgb(40, 28, 22);
-pub const CHARCOAL_BROWN_LIGHT: Color = Color::Rgb(80, 68, 62);
-pub const MOSS_GREEN: Color = Color::Rgb(138, 154, 91);
-// pub const LIGHT_GRAY: Color = Color::Rgb(0xC2, 0xC2, 0xC2);
-pub const LIGHT_RED: Color = Color::Rgb(0xFF, 0xC2, 0xC2);
-pub const LIGHT_GREEN: Color = Color::Rgb(0xC2, 0xFF, 0xC2);
-// pub const LIGHT_BLUE: Color = Color::Rgb(0xC2, 0xC2, 0xFF);
-// pub const DARK_GRAY: Color = Color::Rgb(0x22, 0x22, 0x22);
-// pub const BLACK: Color = Color::Rgb(0x0, 0x0, 0x0);
-// pub const WHITE: Color = Color::Rgb(0xFF, 0xFF, 0xFF);
-// pub const DARK_BLUE: Color = Color::Rgb(0x42, 0x42, 0xFF);
+const CHARCOAL_BROWN: Color = Color::Rgb(60, 48, 42);
+const CHARCOAL_BROWN_DARK: Color = Color::Rgb(40, 28, 22);
+const EARTH_RED: Color = Color::Rgb(0xAD, 0x4B, 0x44);
+const LIGHT_RED: Color = Color::Rgb(0xFF, 0x77, 0x77);
+const FOCUS_GREEN: Color = Color::Rgb(0x17, 0xad, 0x52);
+const DARK_MOSS_GREEN: Color = Color::Rgb(0x2D, 0x33, 0x30);
+const LIGHT_SAND_COLOR: Color = Color::Rgb(255, 223, 186);
+const DEEP_BLUE: Color = Color::Rgb(127, 130, 180);
