960ab5069c9c303805f57eb3b4f6401b8eb8054b
Lucas Dias <lhollowwizard@gmail.com>
2025-06-05 16:24:26 -0300

    feat(clojure): add fast fib function
    It uses memoization to avoid repetitive function calls

--- Clojure/Learning/examples.clj
+++ Clojure/Learning/examples.clj
@@ -6,3 +6,12 @@
     (= x 1) 1
     :else (+ (fib (dec x)) (fib (- x 2)))))
 
+(def fast-fib
+  "Example that uses memoize to make the recursion faster and avoid repetitive fib calls"
+  (memoize
+  (fn fast-fib-impl [n]
+    (cond
+    (= n 0) 0N
+    (= n 1) 1N
+    :else (+ (fast-fib-impl (- n 1)) (fast-fib-impl (- n 2)))))))
+
