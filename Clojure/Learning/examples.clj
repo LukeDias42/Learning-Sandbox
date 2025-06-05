0a6bb3ede64bfa9a985cac5a104ae51a68e2d8dd
Lucas Dias <lhollowwizard@gmail.com>
2025-06-05 16:25:26 -0300

    feat(clojure): add tail call optimization fib function
    This shows how you can use tail call to avoid the risk of overflowing
    the stack with too many calls

--- Clojure/Learning/examples.clj
+++ Clojure/Learning/examples.clj
@@ -15,3 +15,11 @@
     (= n 1) 1N
     :else (+ (fast-fib-impl (- n 1)) (fast-fib-impl (- n 2)))))))
 
+(defn tail-call-fib
+  "Example that uses tail call optimization for calculate the nth fibonacci value."
+  [initial-x]
+    (loop [a 0N b 1N x initial-x]
+      (if (pos? x)
+          (recur b (+ a b) (dec x))
+      a)))
+
